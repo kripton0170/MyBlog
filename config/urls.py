c90d7e56895b9b7fce318ab4f6faa5cf1b3faeff
kripton017 <charles4949@mail.ru>
2022-07-05 14:35:53 +0500

    start

---
+++ config/urls.py
@@ -0,0 +1,15 @@
+from django.conf import settings
+from django.conf.urls.static import static
+from django.contrib import admin
+from django.urls import path, include
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', include('main.urls')),
+    path('comment/', include('comment.urls')),
+    path('contact/'), include('contact.urls'),
+    path('portfolio/'), include('portfolio.urls')
+]
+
+urlpatterns += static (settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+urlpatterns += static (settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
